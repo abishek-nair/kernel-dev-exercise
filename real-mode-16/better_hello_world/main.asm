f73b2cce67fa4dc6811df5adfef295eab5fb6aa5
abishek_nair <phoenix.rulez.93@gmail.com>
2016-10-02 03:54:14 +0530

    The better helloworld is now even better. Call/Ret/ pusha/popa. Just being a good guy in general

--- real-mode-16/better_hello_world/main.asm
+++ real-mode-16/better_hello_world/main.asm
@@ -1,21 +1,27 @@
 [org 0x7C00]
 
+mov BX, the_str
+call printstr
+jmp $
+
+printstr:
 ; Init phase
-mov AH, 0x0E
-mov SI,0
+	pusha
+	mov AH, 0x0E
+	mov SI,0
 
-print_loop:
-	mov AL, [the_str+SI]
-	cmp AL, 0
-	je print_loop_end
-	int 0x10
-	inc SI
+	print_loop:
+		mov AL, [BX+SI]
+		cmp AL, 0
+		je print_loop_end
+		int 0x10
+		inc SI
 	jmp print_loop
+	print_loop_end:
+	popa
+	ret
 
-print_loop_end:
-	jmp $
-
-the_str: db 'Booting Keshiba OS... Complete\nHuhuhaha',0
+the_str: db 'Booting Keshiba OS... Complete',0
 
 times 510-($-$$) db 0
 dw 0xaa55
